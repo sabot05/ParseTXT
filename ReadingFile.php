6ccd6bd33582e0ba3e274e6586b62d4be15b7b0f
Vitaliy <v.yugay@2Gis.ru>
2019-08-15 13:17:58 +0700

    минус плюс

--- ReadingFile.php
+++ ReadingFile.php
@@ -12,34 +12,37 @@ $outputBadWords = [];
 $outputRequiredWords = [];
 $allWordsNew = [];
 $allWordsName = [];
+$minusPlus = ['-' => '', '+' => ''];
 
-//Убираем лишние пробелыы
+//Убираем лишние пробелы и создаем массив
 while ($line = fgets($file)) {
     if (preg_match('/\s/', $line)) {
         $allWords [] = preg_split('/\s/', trim($line));  // массив корпуса
+
     }
 }
 
-//берем первое значение внутреннего массива в качестве индекса массива
-foreach ($allWords as $number => $value) {
+
+//берем первое значение внутреннего массива и записываем в новый массив
+foreach ($allWords as $key => $value) {
     $nameIndex[] = $value[0];
-}
+   }
 
 //меняем индекс на имя первого элемента
-$allWordsName = array_combine($nameIndex, $allWords);
 
-//удаляем первые два элемента в масссиве (слово и цифра)
-foreach ($allWordsName as $key => $value) {
+$allWords = array_combine($nameIndex, $allWords);
+
+//удаляем первые два элемента в массиве (слово и цифра)
+foreach ($allWords as $key => $value) {
     $allWordsNew [$key] = array_splice($value, 2);
+   }
+
+//добавляем два индекса МИНУС и ПЛЮС
+foreach ($allWordsNew as $key => $value) {
+    $allWordsNew[$key] = $minusPlus + $value;
 }
 
-
-
-
 print_r($allWordsNew);
-
-
-
 
 
 
